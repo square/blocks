f33d0aac0647acf9924920ec1c2b2f1ef8bc0b18
Bradley Axen <baxen@squareup.com>
2019-03-12 10:37:27 -0700

    Bump version: 0.5.5 → 0.6.0

--- blocks/__init__.py
+++ blocks/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Bradley Axen"""
 __email__ = 'baxen@squareup.com'
-__version__ = '0.5.5'
+__version__ = '0.6.0'
 
 
 from blocks.core import assemble, iterate, partitioned, divide, place  # NOQA
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ extras_require['dev'] = set(sum(extras_require.values(), []))
 
 setup(
     name='sq-blocks',
-    version='0.5.5',
+    version='0.6.0',
     description=(
         'Blocks provides a simple interface to read, organize, and manipulate structured data'
         ' in files on local and cloud storage'
