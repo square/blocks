9b1a382312d05545186538769d3943383b00aa29
Bradley Axen <baxen@squareup.com>
2019-03-22 10:16:04 -0700

    Bump version: 0.6.1 → 0.6.2

--- blocks/__init__.py
+++ blocks/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Bradley Axen"""
 __email__ = 'baxen@squareup.com'
-__version__ = '0.6.1'
+__version__ = '0.6.2'
 
 
 from blocks.core import assemble, iterate, partitioned, divide, place  # NOQA
